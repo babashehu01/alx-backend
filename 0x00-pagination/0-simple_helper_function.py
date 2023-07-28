ee5213ff33cbc603450ca01cc828e7528d9d6d87
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-07-28 17:10:16 +0100

    Task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def index_range(page, page_size):
+    """
+    Returns a tuple containing the start index and
+    end index corresponding to the range of indexes to return
+    in a list for the given pagination parameters.
+
+    Args:
+        page (int): The current page number (1-indexed).
+        page_size (int): The number of items to display per page.
+
+    Returns:
+        tuple: A tuple containing the start index
+              (inclusive) and end index (exclusive).
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
